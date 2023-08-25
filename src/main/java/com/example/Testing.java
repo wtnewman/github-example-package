8d43fcd0c66d718ca8dce0caea74e921ef8c20ec
Wheeler Newman <wnewman@forthepeople.com>
2023-08-25 15:19:29 -0400

    first commit

---
+++ src/main/java/com/example/Testing.java
@@ -0,0 +1,12 @@
+package com.example;
+
+class Testing {
+
+    public static void display(String param){
+        System.out.println("You parameter is " + param);
+    }
+
+    public void someFunc(){
+        System.out.println("Testing.someFunc");
+    }
+}
